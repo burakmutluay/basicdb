5fdacb9323437cf99d6ccb8ed2b4058b0bfe39b2
Burak Mutluay <mburakmutluay@gmail.com>
2022-08-29 16:30:44 +0300

    tuple methods are defined

--- src/main/java/basicdb/tuple/Tuple.java
+++ src/main/java/basicdb/tuple/Tuple.java
@@ -6,8 +6,35 @@ public class Tuple implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private TupleDesc tupleDesc;
+    private RecordId recordId;
+    private Field[] fields;
+
     public Tuple(TupleDesc tupleDesc){
         throw new UnsupportedOperationException("implement me!");
     }
 
+    public TupleDesc getTupleDesc() {
+        throw new UnsupportedOperationException("implement me!");
+    }
+
+    public void setField(int i, Field f) {
+        throw new UnsupportedOperationException("implement me!");
+    }
+
+    public Field getField(int i) {
+        throw new UnsupportedOperationException("implement me!");
+    }
+
+    public String toString() {
+        throw new UnsupportedOperationException("implement me!");
+    }
+
+    public RecordId getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(RecordId recordId) {
+        this.recordId = recordId;
+    }
 }
